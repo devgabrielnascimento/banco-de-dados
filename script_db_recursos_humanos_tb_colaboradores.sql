59c2001734a91ecd47da9fc27d00329654c21630
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-03-05 11:29:42 -0300

    atualizando...

--- script_db_recursos_humanos_tb_colaboradores.sql
+++ script_db_recursos_humanos_tb_colaboradores.sql
@@ -15,7 +15,7 @@ CREATE TABLE tb_colaboradores(
 -- DML's 
 INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Gabriel","401.868.448-64","atendente",1400.20,"2024-03-01");
 INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Maria","311.838.402-74","psicóloga",2400.20,"2024-03-02");
-INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Lucas","111.881.401-34","psicologo",2400.20,"2024-03-03");
+INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Lucas","111.881.401-34","psicólogo",2400.20,"2024-03-03");
 INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Roberta","104.856.412-41","secretária",1750.20,"2024-03-04");
 INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Marcos","201.648.401-12","analista jr",2100.70,"2024-03-05");
 INSERT INTO tb_colaboradores (nome, cpf, ocupacao, salario, data_admissao) VALUES ("Luara","100.561.530-51","analista pleno",5400.00,"2024-03-06");
